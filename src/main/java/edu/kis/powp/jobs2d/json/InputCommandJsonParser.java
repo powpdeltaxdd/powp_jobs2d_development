c4c33df4f93fed710caca46cb2594970dba203cb
mkarpacka <209348@edu.p.lodz.pl>
2019-05-29 15:46:52 +0200

    parsing added but not working yet

--- src/main/java/edu/kis/powp/jobs2d/json/InputCommandJsonParser.java
+++ src/main/java/edu/kis/powp/jobs2d/json/InputCommandJsonParser.java
@@ -1,17 +1,48 @@
 package edu.kis.powp.jobs2d.json;
 import org.json.*;
 
+import java.util.ArrayList;
+
 public class InputCommandJsonParser {
 
-    InputCommandJsonParser(){
-        JSONObject obj = new JSONObject(" .... ");
-        String pageName = obj.getJSONObject("pageInfo").getString("pageName");
+    InputCommandJsonParser(String jsonStrong){
 
-        JSONArray arr = obj.getJSONArray("posts");
-        for(int i = 0; i < arr.length(); i++)
+
+        JSONObject obj = new JSONObject(jsonStrong);
+//        String x = obj.getJSONObject("coordinates").getString("x");
+
+        JSONArray commandArr = obj.getJSONArray("command");
+
+
+
+        for(int i = 0; i < commandArr.length(); i++)
         {
-            String post_id = arr.getJSONObject(i).getString("post_id");
+            int x = commandArr.getJSONObject(i).getInt("x");
+
+            int y = commandArr.getJSONObject(i).getInt("y");
+            String command = commandArr.getJSONObject(i).getString("command");
+            System.out.println(x + y + command);
         }
     }
 
+
+
+    public static void main(String[] args) {
+        String temp = " {\n"
+                      + "        \"command\":[{\n"
+                      + "        \"x\": 10,\n"
+                      + "        \"y\": 20,\n"
+                      + "        \"operator\":\"OperateTo\"}]\n"
+                      + "        }";
+
+        InputCommandJsonParser par = new InputCommandJsonParser(temp);
+
+    }
 }
+
+//{
+//        "command":[{
+//        "x": 10,
+//        "y": 20,
+//        "operator":"OperateTo"}]
+//        }
